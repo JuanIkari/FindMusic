16f2ba68ef021b8bd9823329fa4ea2ded2bf509b
Alex <a.gonzalez5@utp.edu.co>
2024-09-24 22:41:45 -0500

    Transparencia para BottomTab y NavBar

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Icon from "react-native-vector-icons/Ionicons";
+import * as NavigationBar from "expo-navigation-bar";
 
 import Feed from "./Feed";
 import Buscar from "./Buscar";
@@ -8,11 +9,22 @@ import Perfil from "./Perfil";
 import Playlist from "./Playlist";
 
 const Tab = createBottomTabNavigator();
+NavigationBar.setPositionAsync("absolute");
+NavigationBar.setBackgroundColorAsync("#ffffff01");
 
 export default function Home() {
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
+        tabBarStyle: {
+          borderTopWidth: 0,
+          backgroundColor: 'transparent',
+          position: 'absolute',
+          left: 0,
+          bottom: 0,
+          right: 0,
+          elevation: 0,
+        },
         tabBarIcon: ({ color, size }) => {
           let iconName;
 
@@ -32,10 +44,10 @@ export default function Home() {
         tabBarInactiveTintColor: "gray",
       })}
     >
-      <Tab.Screen name="Feed" component={Feed} options={{headerShown: false}}/>
-      <Tab.Screen name="Buscar" component={Buscar} options={{headerShown: false}}/>
-      <Tab.Screen name="Perfil" component={Perfil} options={{headerShown: false}}/>
-      <Tab.Screen name="Playlist" component={Playlist} options={{headerShown: false}}/>
+      <Tab.Screen name="Feed" component={Feed} options={{ headerShown: false }} />
+      <Tab.Screen name="Buscar" component={Buscar} options={{ headerShown: false }} />
+      <Tab.Screen name="Perfil" component={Perfil} options={{ headerShown: false }} />
+      <Tab.Screen name="Playlist" component={Playlist} options={{ headerShown: false }} />
     </Tab.Navigator>
   );
 }
