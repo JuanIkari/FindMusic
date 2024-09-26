15b5d0934da8175947d205909238d01c1ff60969
Alex <a.gonzalez5@utp.edu.co>
2024-09-26 12:40:55 -0500

    Logo añadido

--- screens/Register.js
+++ screens/Register.js
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { Button, View, Text } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { Image } from 'expo-image';
 
 export default function Register({ navigation }) {
   return (
     <LinearGradient colors={["#0C0322", "#190633"]} style={{ flex: 1 }}>
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text> REGISTRARSE </Text>
+        <Image 
+          source={'https://i.ibb.co/0V0J1pK/1727370200014.png'}
+          style={{ width: 200, height: 200 }}
+        />
         <Button
           title="Iniciar sesion"
           onPress={() => navigation.navigate("Login")}
